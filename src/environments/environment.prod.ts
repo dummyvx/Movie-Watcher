cafaa7fa7b9a908781b13d1913464641302c581a
Michal Bartosik <msbartosik@gmail.com>
2021-02-14 09:45:34 +0100

    testing config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  api_key: process.env.API_KEY,
+  api_key: `${process.env.API_KEY}`,
   tmdb_base_url: 'https://api.themoviedb.org/3',
   tmdb_imagesUrl_w300: 'http://image.tmdb.org/t/p/w300',
   tmdb_imagesUrl_w500: 'http://image.tmdb.org/t/p/w500',
-  backend_base_url: process.env.BACKEND_BASE_URL
+  backend_base_url: `${process.env.BACKEND_BASE_URL}`
 };
